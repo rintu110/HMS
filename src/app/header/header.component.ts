b4a857ad12bc8329ccfd5629ac0b3f8cc907a381
rinkunaik003 <rinkunaik2008@gmail.com>
2024-02-01 17:28:36 +0530

    home page design

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject, TemplateRef } from '@angular/core';
+import { NgbOffcanvas, OffcanvasDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  private offcanvasService = inject(NgbOffcanvas);
+	closeResult = '';
+
+	open(content: TemplateRef<any>) {
+		this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' }).result.then(
+			(result) => {
+				this.closeResult = `Closed with: ${result}`;
+			},
+			(reason) => {
+				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+			},
+		);
+	}
+
+	private getDismissReason(reason: any): string {
+		switch (reason) {
+			case OffcanvasDismissReasons.ESC:
+				return 'by pressing ESC';
+			case OffcanvasDismissReasons.BACKDROP_CLICK:
+				return 'by clicking on the backdrop';
+			default:
+				return `with: ${reason}`;
+		}
+	}
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,58 @@
+import { Component, inject} from '@angular/core';
+import { HomegalleryComponent } from '../homegallery/homegallery.component';
+import { NgbCarouselModule, NgbCalendar, NgbDate, NgbDateParserFormatter,  NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HomegalleryComponent, NgbCarouselModule, NgbDatepickerModule, FormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent{
+  calendar = inject(NgbCalendar);
+	formatter = inject(NgbDateParserFormatter);
+
+	hoveredDate: NgbDate | null = null;
+	fromDate: NgbDate | null = this.calendar.getToday();
+	toDate: NgbDate | null = this.calendar.getNext(this.calendar.getToday(), 'd', 10);
+
+	onDateSelection(date: NgbDate) {
+		if (!this.fromDate && !this.toDate) {
+			this.fromDate = date;
+		} else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
+			this.toDate = date;
+		} else {
+			this.toDate = null;
+			this.fromDate = date;
+		}
+	}
+
+	isHovered(date: NgbDate) {
+		return (
+			this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
+		);
+	}
+
+	isInside(date: NgbDate) {
+		return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+	}
+
+	isRange(date: NgbDate) {
+		return (
+			date.equals(this.fromDate) ||
+			(this.toDate && date.equals(this.toDate)) ||
+			this.isInside(date) ||
+			this.isHovered(date)
+		);
+	}
+
+	validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+		const parsed = this.formatter.parse(input);
+		return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
+	}
+
+
+}
+
